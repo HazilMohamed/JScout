edee76b5ee7bb2d3238fa0e0870dc8a819930cc3
hazilMohamed <mohamedhazil14@yahoo.in>
2021-03-02 11:02:35 +0530

    added POST request to fetch data

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,15 @@
-// const express = require("express");
 import express from "express";
-import { PythonShell } from "python-shell";
+
+import matchRouter from "./match/getMatchDetails";
 
 const app = express();
 const port = 8080;
 
-app.get("/match", (req, res) => {
-  res.send("Hello world!");
-  const pythonOptions = {
-    args: [String(req.query.minute)],
-    scriptPath: "./src/python/",
-  };
-  PythonShell.run("./match.py", pythonOptions, function (err, out) {
-    if (err) console.log(err);
-    if (out) console.log(out);
-  });
+app.get("/", (req, res) => {
+  res.send("hello from root");
 });
+
+app.use("/match", matchRouter);
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
---
+++ src/js/match/getMatchDetails.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { PythonShell } from "python-shell";
+const matchRouter = express.Router();
+
+matchRouter.get("/", (req, res) => {
+  res.send("hello from match");
+});
+
+matchRouter.post("/", (req, res) => {
+  const pythonOptions = {
+    args: [String(req.query.minute)],
+    scriptPath: "./src/python/",
+  };
+  PythonShell.run("./match.py", pythonOptions, function (err, out) {
+    if (err) {
+      res.json(err);
+    }
+    if (out) {
+      res.json(out);
+    }
+  });
+});
+
+export default matchRouter;
